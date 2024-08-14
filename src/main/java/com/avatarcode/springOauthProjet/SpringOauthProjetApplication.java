03d32f2a92d54a052a5379ab49e61f7c9e43eb68
=wiltord911 <=>
2024-08-14 14:47:10 +0100

    first commit

---
+++ src/main/java/com/avatarcode/springOauthProjet/SpringOauthProjetApplication.java
@@ -0,0 +1,13 @@
+package com.avatarcode.springOauthProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avatarcode/springOauthProjet/SpringOauthProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avatarcode.springOauthProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
